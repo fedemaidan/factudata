29facad434b325d9de1855ae414ea324ffa0af2d
Fede Maidan <fedemaidan@gmail.com>
2023-06-13 11:29:06 -0300

    add functions framework

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import argparse
 import os
 import sys
 import csv
+import functions_framework
 
+@functions_framework.http
 def hello_world(request):
     """Responds to any HTTP request.
     Args:
